9c4d6d77ab36e2592644040189a04821c3364f0c
paulohfreire <paulo.freire53@gmail.com>
2021-07-04 08:15:06 -0300

    add router and product model

--- rotas/produtos.rota.js
+++ rotas/produtos.rota.js
@@ -1,61 +1,51 @@
 const express = require('express')
 const router = express.Router()
-const { v4: uuidv4 } = require('uuid')
 const produtoMid = require('../middleware/validarProduto.middleware')
-const produtoSchema = require('../schemas/produto.schema')
-
-const produtos = {}
+const { Produto } = require('../models')
 
 router.post('/', produtoMid)
 router.put('/', produtoMid)
 
-router.get('/:id', (req, res) => {
-    res.json({ produtos: produtos[req.params.id] })
+router.get('/', async (req, res) => {
+    const produtos = await Produto.findAll()
+    res.json({ produtos: produtos })
 })
 
-router.put('/', (req, res) => {
+router.get('/:id', async (req, res) => {
+    const produto = await Produto.findByPk(req.params.id)
+    res.json({ produtos: produto })
+})
+
+router.post('/', async (req, res) => {
+    const produto = await Produto.create(req.body)
+    res.json({ msg: "Produto adicionado com sucesso!" })
+})
+
+router.delete('/', async (req, res) => {
     const id = req.query.id
-    if (id && produtos[id]) {
-        const produto = req.body
-        produto.id = id
-        produtos[id] = produto
+    const produto = await Produto.findByPk(id)
+
+    if (produto) {
+        await produto.destroy()
+        res.json({ msg: "Produto excluído com sucesso." })
+    } else {
+        res.status(400).json({ msg: "Produto não encontrado." })
+    }
+})
+
+router.put('/', async (req, res) => {
+    const id = req.query.id
+    const produto = await Produto.findByPk(id)
+
+    if (produto) {
+        produto.nome = req.body.nome
+        produto.descricao = req.body.descricao
+        produto.valor = req.body.valor
+        await produto.save()
         res.json({ msg: "Produto atualizado com sucesso!" })
     } else {
         res.status(400).json({ msg: "Produto não encontrado!" })
     }
-})
-
-router.delete('/', (req, res) => {
-    const id = req.query.id
-    if (id && produtos[id]) {
-        delete produtos[id]
-        res.json({ msg: "Produto deletado com sucesso!" })
-    } else {
-        res.status(400).json({ msg: "Produto não encontrado!" })
-    }
-})
-
-router.post('/', (req, res) => {
-    const produto = req.body
-    const Ajv = require('ajv')
-    const ajv = new Ajv()
-    const addFormats = require("ajv-formats")
-    addFormats(ajv)
-    const validate = ajv.compile(produtoSchema)
-    const valid = validate(produto)
-
-    if (valid) {
-        const idproduto = uuidv4()
-        produto.id = idproduto
-        produtos[idproduto] = produto
-        res.json({ msg: "Produto adicionado com sucesso!" })
-    } else {
-        res.status(400).json({ msg: "Dados inválidos", errors: validate.errors })
-    }
-})
-
-router.get('/', (req, res) => {
-    res.json({ produtos: Object.values(produtos) })
 })
 
 module.exports = router
